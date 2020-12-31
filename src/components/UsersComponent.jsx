33e8886f4df5ca783cf44ce49bddedb9562e4920
husseinmansour6 <hmansour@aldogroup.com>
2020-12-30 19:14:54 -0500

    imported the necessary data in order to dispatch and action to get the users and show their cards

--- src/components/UsersComponent.jsx
+++ src/components/UsersComponent.jsx
@@ -1,10 +1,32 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useSelector, useDispatch } from "redux"
+import { getUsers } from "../redux/actions/users"
 import Card from "./CardComponent"
 
 const Users = () => {
+  // useEffect to handle the action and dispatch it in our component
+  const dispatch = useDispatch()
+  const users = useSelector(state => state.users.users)
+
+  useEffect(() => {
+    dispatch(
+      getUsers([
+        {
+          id: 1,
+          name: "blah blah",
+          company: {
+            name: "Company1",
+            catchPhrase: "hey there"
+          }
+        }
+      ])
+    )
+  }, [])
   return (
     <>
-      <Card />
+      {users.length > 0 &&
+        users.map(user => <Card user={user} key={user.id} />)}
+      {users.length === 0 && <p>No users available!</p>}
     </>
   )
 }
